8666b2fe30aecfe52a0140450647e6b2de3551e9
KISHALOY MALLICK <kishaloymallick347@gmail.com>
2016-11-26 21:23:24 -0500

    Update demo1.ino

--- Arduino/demo1/demo1.ino
+++ Arduino/demo1/demo1.ino
@@ -1,18 +1,2 @@
-/*
- First program
- */
- int ledPin = 13;
- void setup()
- {
- //initialize pins as outputs
- pinMode (ledPin, OUTPUT);
- }
- 
- void loop()
- {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
- }
+
 
